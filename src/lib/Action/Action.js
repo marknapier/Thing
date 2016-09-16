2c0b6bb34b481ca95eda50941432647891c0722f
Mark Napier <napiernapier@gmail.com>
2016-09-16 08:08:35 -0400

    MN: WIP try out a couple of demos (very rough)

---
+++ src/lib/Action/Action.js
@@ -0,0 +1,150 @@
+
+class Action {
+	constructor() {
+	}
+
+	init (props) {
+		this.props = props || {};
+		return this;
+	}
+
+	go () {
+		window.console.log('Action.go()');
+	}
+
+	stop () {
+		window.console.log('Action.stop()');
+	}
+
+	static make () {
+	  var cls = this;
+	  var instance = new cls();
+	  instance.init.apply(instance, arguments);
+	  return instance;
+	}
+}
+
+// module.exports = Action;
+
+class Timer extends Action {
+	init (props) {
+		props = props || {};
+		this.callback = props.callback || function () {};
+		this.delay = props.delay || 1000;
+		this.timerID = null;
+		return this;
+	}
+
+	go () {
+		clearTimeout(this.timerID);
+		this.timerID = setTimeout(this.callback, this.delay);
+		return this;
+	}
+
+	stop () {
+		clearTimeout(this.timerID);
+		this.timerID = null;
+	}
+}
+
+
+
+class Pulsar extends Action {
+	init (props) {
+		props = props || {};
+		this.callback = props.callback || function () {};
+		this.delay = props.delay || 1000;
+		this.T = Timer.make({callback: this.trigger.bind(this), delay: this.delay});
+		return this;
+	}
+
+	go () {
+		this.T.go();
+		return this;
+	}
+
+	stop () {
+		this.T.stop();
+	}
+
+	trigger () {
+		this.callback();
+		this.T.go();
+	}
+}
+
+
+
+var PI = 3.14159265359;
+var HALFPI = PI/2.0;
+
+class Rand {
+	static randItem(arr) {
+		if (arr && arr.length > 0) {
+			return arr[ Rand.randInt(0, arr.length-1) ];
+		}
+	}
+
+	// Returns a random integer between min (included) and max (included)
+	// Using Math.round() will give you a non-uniform distribution!
+	static randInt(min, max) {
+		min = Math.ceil(min||0);
+		max = Math.floor(max||1);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	// return float between 0 and .999999
+	static randfloat() {
+	    return Math.random();
+	}
+
+	static randPercent(threshold) {
+		return Rand.randInt(1,100) < threshold;
+	}
+
+	// random integer within maxDistance of target (distributed in a bell curve around target)
+	static randCloseTo(target, maxDistance) {
+		// return target + (maxDistance * randNormal());    // concentrated towards center 50% of range
+		// return target + (maxDistance * randSin2());   // spread over entire range, somewhat concentrated towards center 
+		return target + (maxDistance * Rand.randPow2());   // spread over entire range, with sharp concentration around center
+	}
+
+	// return float between 0 and 1, distributed exponentially closer to 0
+	static randPow() {
+		return Math.pow(1.0 - Rand.randfloat(), 4);
+	}
+
+	// return float between 0 and 1, distributed toward 1
+	static randSin() {
+		return Math.sin(Rand.randfloat() * HALFPI);
+	}
+
+	// return float between -1 and 1, distributed exponentially closer to 0
+	static randPow2() {
+		return Rand.randPow() - Rand.randPow();
+	}
+
+	// return float between -1 and 1, distributed in a bell curve around 0
+	static randNormal() {
+		return ((Rand.randfloat() + Rand.randfloat() + Rand.randfloat() + Rand.randfloat() + Rand.randfloat() + Rand.randfloat()) - 3.0) / 3.0;
+	}
+
+	// return float between -1 and 1, distributed closer to 0
+	static randSin2() {
+		return Rand.randSin() - Rand.randSin();
+	}
+}
+
+
+var colors = ['yellow', 'red', 'blue', 'green', 'magenta', 'cyan', 'orange', 'black'];
+var Box = Thing.getClass('Box');
+var b = Box.make({w:50, h:50, transition:'background-color 1.5s', position:'relative', margin:'5px'});
+var p = Pulsar.make({
+	callback: function () {
+		b.css({backgroundColor: Rand.randItem(colors)});
+	}, 
+	delay: 2000
+});
+b.render();
+p.go();
+
