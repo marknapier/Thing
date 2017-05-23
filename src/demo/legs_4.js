3a172b6d12974a605433702953f1bc909016b7b8
Mark Napier <napiernapier@gmail.com>
2017-05-23 00:05:48 -0400

    MN: Legs 4. Use translate3d(x,y,z) in transforms. Add darkspot to Legs 3.

--- src/demo/legs_4.js
+++ src/demo/legs_4.js
@@ -5,61 +5,61 @@ function makeBunchOfLegs (width) {
   var Rand = Thing.classes.Rand;
   var Img = Thing.classes.Img;
   var arrayOLegs = [
-    Img.make({
-      src: 'img/leg_durer_adam_right_and_foot_1.png',
-      rotate: {y: Rand.randInt(-45, 45)},
-      x: Rand.randInt(0,width),
-      y: 1250,
-      w: 800
-    }),
-    Img.make({
-      src: 'img/leg_durer_eve_vase_1.png',
-      rotate: {y: Rand.randInt(-45, 45)},
-      x: Rand.randInt(0,width),
-      y: 1200,
-      w: 800
-    }),
-    Img.make({
-      src: 'img/leg_durer_right_1.png',
-      rotate: {y: Rand.randInt(-45, 45)},
-      x: Rand.randInt(0,width),
-      y: 900,
-      w: 700
-    }),
-    Img.make({
-      src: 'img/leg_titian_venus_right_1.png',
-      rotate: {y: Rand.randInt(-45, 45)},
-      x: Rand.randInt(0,width),
-      y: 1000,
-      w: 600
-    }),
-    Img.make({
-      src: 'img/titian_venus_both_legs_1.png',
-      rotate: {y: Rand.randInt(-45, 45)},
-      x: Rand.randInt(0,width),
-      y: 500,
-      w: 750
-    }),
-    Img.make({
-      src: 'img/leg_eve_left_1.png',
-      x: Rand.randInt(0,width),
-      y: 1200,
-      w: 600
-    }),
-    Img.make({         // Big Leg
-      src: 'img/leg_eve_left_1.png',
-      rotate: {y: Rand.randInt(-45, 45)},
-      x: Rand.randInt(0,width),
-      y: 300,
-      w: 900
-    }),
-    Img.make({
-      src: 'img/titian_venus_both_legs_1.png',
-      rotate: {y: Rand.randInt(-45, 45)},
-      x: Rand.randInt(0,width),
-      y: Rand.randInt(-100,500),
-      w: 750
-    }),
+    // Img.make({
+    //   src: 'img/leg_durer_adam_right_and_foot_1.png',
+    //   rotate: {y: Rand.randInt(-45, 45)},
+    //   x: Rand.randInt(0,width),
+    //   y: 1250,
+    //   w: 800
+    // }),
+    // Img.make({
+    //   src: 'img/leg_durer_eve_vase_1.png',
+    //   rotate: {y: Rand.randInt(-45, 45)},
+    //   x: Rand.randInt(0,width),
+    //   y: 1200,
+    //   w: 800
+    // }),
+    // Img.make({
+    //   src: 'img/leg_durer_right_1.png',
+    //   rotate: {y: Rand.randInt(-45, 45)},
+    //   x: Rand.randInt(0,width),
+    //   y: 900,
+    //   w: 700
+    // }),
+    // Img.make({
+    //   src: 'img/leg_titian_venus_right_1.png',
+    //   rotate: {y: Rand.randInt(-45, 45)},
+    //   x: Rand.randInt(0,width),
+    //   y: 1000,
+    //   w: 600
+    // }),
+    // Img.make({
+    //   src: 'img/titian_venus_both_legs_1.png',
+    //   rotate: {y: Rand.randInt(-45, 45)},
+    //   x: Rand.randInt(0,width),
+    //   y: 500,
+    //   w: 750
+    // }),
+    // Img.make({
+    //   src: 'img/leg_eve_left_1.png',
+    //   x: Rand.randInt(0,width),
+    //   y: 1200,
+    //   w: 600
+    // }),
+    // Img.make({         // Big Leg
+    //   src: 'img/leg_eve_left_1.png',
+    //   rotate: {y: Rand.randInt(-45, 45)},
+    //   x: Rand.randInt(0,width),
+    //   y: 300,
+    //   w: 900
+    // }),
+    // Img.make({
+    //   src: 'img/titian_venus_both_legs_1.png',
+    //   rotate: {y: Rand.randInt(-45, 45)},
+    //   x: Rand.randInt(0,width),
+    //   y: Rand.randInt(-100,500),
+    //   w: 750
+    // }),
     Img.make({
       src: 'img/leg_eve_left_1.png',
       rotate: {y: Rand.randInt(-45, 45)},
@@ -100,6 +100,18 @@ function makeBunchOfLegs (width) {
   return arrayOLegs;
 }
 
+function makeMarker (props) {
+  let defaultProps = {
+    backgroundColor: '#0d0',
+    w: 100,
+    h: 100,
+    color: '#000',
+    fontSize: '40px'
+  };
+  props = $.extend({}, defaultProps, props);
+  return Thing.make(props);
+}
+
 $(function () {
   var Img = Thing.classes.Img;
   var Rand = Thing.classes.Rand;
@@ -110,7 +122,7 @@ $(function () {
   var pixelHeight = pixelWidth * aspectRatio;
   var mainScale = pixelWidth * 0.001;  // assume design is 1000 pixels wide, this will be 1
   var background = Meninas.makeBackground(pixelWidth, pixelHeight, mainScale).css({backgroundColor:'rgb(124, 10, 0)'});
-  var legContainerWidth = Thing.classes.Rand.randInt(2000,3000);
+  var legContainerWidth = 4000;
 
   var rightWall = Img.make({
       src: 'img/vintagewallpaper4_crop.png',
@@ -152,25 +164,59 @@ $(function () {
 
   // put all the legs into one box
   var legContainer = Thing.classes.Box.make({
-    x: 50,
-    y: 50,
+    x: 100,
+    y: 100,
     w: legContainerWidth,
     h: 3100,
-    transformStyle: 'preserve-3d'
+    transformStyle: 'preserve-3d',
+    border: '8px dotted #696'
   })
-  .add(makeBunchOfLegs(legContainerWidth));
+  .add(makeBunchOfLegs(legContainerWidth))
+  .add(makeMarker({x:0, y:0}))
+  .add(makeMarker({x:0, y:3100-100, z:-500}))
+  .add(makeMarker({x:legContainerWidth-100, y:3100-100, z: -500}))
+  .add(makeMarker({x:legContainerWidth-100, y:3100-100, z: 500}))
+  .add(Thing.classes.Box.make({  // floor
+      x: 0,
+      y: 3100 - (2000/2),
+      w: legContainerWidth,
+      h: 2000,
+      rotate: {x:90},
+      border: '12px dashed #0f3',
+      backgroundColor: 'rgba(150,0,0,0.4)'
+    }))
+  .add(Thing.classes.Box.make({   // back wall
+      x: 0,
+      y: 0,
+      z: -1000,
+      w: legContainerWidth,
+      h: 3100,
+      border: '12px dashed #0f3',
+      backgroundColor: 'rgba(0,150,0,0.4)'
+    }))
+  .add(Thing.classes.Box.make({   // right wall
+      x: legContainerWidth - (2000/2),
+      y: 0,
+      z: 0,
+      w: 2000,
+      h: 3100,
+      rotate: {y:-90},
+      border: '12px dashed #0f3',
+      backgroundColor: 'rgba(0,0,150,0.4)'
+    }))
+  ;
 
   // another bunch of legs
-  var bunchOfLegs = Thing.classes.Box.make({
-    x: 3300,
-    y: 300,
-    w: legContainerWidth * 0.75,
-    h: 2000,
-    transformStyle: 'preserve-3d',
-    scale: 0.7,
-    rotate: {z:-5}
-  })
-  .add(makeBunchOfLegs(legContainerWidth/2));
+  // var bunchOfLegs = Thing.classes.Box.make({
+  //   x: 3300,
+  //   y: 300,
+  //   w: legContainerWidth * 0.75,
+  //   h: 2000,
+  //   transformStyle: 'preserve-3d',
+  //   scale: 0.7,
+  //   rotate: {z:-5}
+  // })
+  // .add(makeBunchOfLegs(legContainerWidth/2));
 
   // random dark circle on floor
   var darkspot = Thing.classes.Circle.make({
@@ -187,7 +233,7 @@ $(function () {
   floor.add(darkspot);
   background.add(wallpaper);
   background.add(backWall);
-  background.add(bunchOfLegs);
+  // background.add(bunchOfLegs);
   background.add(rightWall);
   background.add(edge);
   background.add(floor);
@@ -199,28 +245,6 @@ $(function () {
 
 
 /*
-
--webkit-mask-image: url(img/las_meninas_girl_mask.png);
--webkit-mask-repeat: no-repeat;
--webkit-mask-size: 100%;
-opacity: 0.85
-
-
-// Shadow div - added to the leg container div
-<div style="
-width: 500px;
-height: 950px;
-position:absolute;
-transform-origin: 50% 50%;
-transform: translate3D(2400px, 2900px, -400px) rotateX(89deg) rotateZ(30deg);
-background-image: radial-gradient(ellipse at 50% 50%, rgb(109, 96, 96) 0%, #8a7777 35%, transparent 68%);
-opacity: 0.85; ">
-</div>
-
-// translate:  X to the leg to be shadowed
-//    Y to the floor height - half the shadow height
-
-
 // striped shadow-ish shape on floor
 <div class="Img" id="Img21" style="position: absolute;width: 1800px;height: 700px;-webkit-mask-image: url(file:///C:/Users/Mark/GIT/htdocs/Thing/img/shadow_mask_3.png);-webkit-mask-repeat: no-repeat;-webkit-mask-size: 100%;background-image: linear-gradient(90deg, transparent 51%, #ffffc8 51%, #ffffc8 59%, transparent 59%),      linear-gradient(90deg, transparent 43%, #f33054 43%, #f33054 67%, transparent 67%),      linear-gradient(90deg, #029b4f 34%, #262626 34%, #262626 75%, #029b4f 75%);background-position: 10%, 10%, 10%;background-size: 15%, 15%, 15%;transform: translate3d(2100px, 2788px, -100px) rotateX(84deg) rotateZ(-23deg);opacity: 0.85;"></div>
 <div class="Img" id="Img21" style="position: absolute;width: 1800px;height: 700px;-webkit-mask-image: url(file:///C:/Users/Mark/GIT/htdocs/Thing/img/shadow_mask_1.png);-webkit-mask-repeat: no-repeat;-webkit-mask-size: 100%;background-image: linear-gradient(90deg, transparent 21%, #af2828 21%, #d0f9c5 59%, transparent 59%),      linear-gradient(90deg, transparent 43%, #d930f3 43%, #f33054 67%, transparent 67%),      linear-gradient(90deg, #002177 34%, #000f90 34%, #0b009c 75%, #01045a 75%);background-position: 10%, 10%, 10%;background-size: 15%, 15%, 15%;transform: translate3d(600px, 2707px, 38px) rotateX(84deg) rotateZ(-36deg);opacity: 0.85;"></div>
@@ -228,5 +252,4 @@ opacity: 0.85; ">
 // Darker striped shadow-ish shape on floor
 <div class="Img" id="Img21" style="position: absolute;width: 2300px;height: 800px;-webkit-mask-image: url(file:///C:/Users/Mark/GIT/htdocs/Thing/img/shadow_mask_3.png);-webkit-mask-repeat: no-repeat;-webkit-mask-size: 100%;background-image: linear-gradient(90deg, transparent 51%, #3a3a17 51%, #000000 59%, transparent 59%),      linear-gradient(90deg, transparent 43%, #331a1f 43%, #691f2d 67%, rgba(39, 5, 5, 0) 67%),      linear-gradient(90deg, #001b0d 34%, #262626 34%, #262626 75%, #001d0f 75%);background-position: 10%, 10%, 10%;background-size: 15%, 15%, 15%;transform: translate3d(1500px, 3000px, -734px) rotateX(80deg) rotateZ(-9deg);opacity: 0.85;"></div>
 <div class="Img" id="Img21" style="position: absolute;width: 1800px;height: 700px;-webkit-mask-image: url(file:///C:/Users/Mark/GIT/htdocs/Thing/img/shadow_mask_1.png);-webkit-mask-repeat: no-repeat;-webkit-mask-size: 100%;background-image: linear-gradient(90deg, transparent 21%, #210d0d 21%, #041001 59%, transparent 59%),      linear-gradient(90deg, transparent 43%, #28152b 43%, #2d0d13 67%, transparent 67%),      linear-gradient(90deg, #002177 34%, #000f90 34%, #0b009c 75%, #01045a 75%);background-position: 10%, 10%, 10%;background-size: 15%, 15%, 15%;transform: translate3d(700px, 3000px, -1362px) rotateX(80deg) rotateZ(-16deg);opacity: 0.85;"></div>
-
 */
