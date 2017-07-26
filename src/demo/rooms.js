7d525191ca95420e3026561cd5cacd4f43a03ce8
Mark Napier <napiernapier@gmail.com>
2017-07-25 21:43:52 -0400

    MN: Adjust perspective and dimensions in rooms.js

--- src/demo/rooms.js
+++ src/demo/rooms.js
@@ -2,6 +2,12 @@ var Thing = window.Thing;
 var Meninas = window.Meninas;
 var Rand = Thing.classes.Rand;
 
+var pageParams = Thing.classes.Page.getParams();
+var aspectRatio = 0.620;
+var CW = pageParams.canvasWidth || 6000;  // canvas width
+var CH = CW * aspectRatio;
+
+
 function makeLump (props) {
   var imgNames = [
     'birth_of_venus_leg_left.png',
@@ -13,14 +19,15 @@ function makeLump (props) {
   var imgs = imgNames.map( function (imgName) {
     return Thing.classes.Img.make({
       src: 'img/' + imgName,
-      x: Rand.randNormal() * 100,
-      y: Rand.randInt(0, 800),
-      w: Rand.randInt(500, 1000),
-      h: Rand.randInt(500, 2000),
+      x: Rand.randNormal() * (CW * 0.025),
+      y: Rand.randInt(0, CW * 0.15),
+      w: Rand.randInt(CW * 0.1, CW * 0.2),
+      h: Rand.randInt(CW * 0.1, CW * 0.3),
       opacity: 0.1 + (Rand.randFloat()*0.9),
       filter: 'blur(' +(Rand.randPow() * 20.0).toFixed(1)+ 'px)',
     });
   });
+
   return Thing.classes.Box.make({
     x: 0,
     y: 500,
@@ -96,70 +103,46 @@ function fillFloor () {
 }
 
 $(function () {
-  var pageParams = Thing.classes.Page.getParams();
-  var aspectRatio = 0.620;
-  var pixelWidth = pageParams.canvasWidth || 6000;
-  var pixelHeight = pixelWidth * aspectRatio;
 
   var lightSpot = Thing.make({
-    w: 4150,
-    h: 2800,
+    width: '100%',
+    height: '100%',
     background: 'radial-gradient(at 40% 30%, rgba(255, 255, 255, 0.3) 10%, rgba(94, 72, 82, 0.54) 90%)'
   });
 
-  var backWall = Thing.classes.Box.make({
-    background: 'linear-gradient(90deg, rgba(255, 244, 156, 0.3) 4px, transparent 4px)',
-    backgroundSize: '8px',
-    w: 5000,
-    h: 2800,
-    overflow: 'hidden',
-  });
-  backWall.add(lightSpot);
-
   var legRoom = makeRoom({
-    x: pixelWidth * 0.6,
-    y: pixelHeight * 0.2,
-    w: pixelWidth * 0.2,
-    h: pixelHeight * 0.79
+    x: CW * 0.45,
+    y: CH * 0.208,
+    z: CH * -0.2,
+    w: CW * 0.2,
+    h: CH * 0.79,
+    d: CW * 0.2
   });
   legRoom.add(makeLump());
-  legRoom.rotate({y: -25});
+  legRoom.rotate({y: 25});
+  legRoom.bottom.add(Thing.classes.BGImg.make({
+    url:'img/persian_carpet_fine_red_1.png'
+  }));
 
-  var corridor = makeRoom({
-    x: 4150,
-    y: -993,
-    h: 3735,
-    w: 850,
-    d: 800,
+  var corridor = makeWrappedRoom({
+    x: CW * 0.819,
+    y: 0,
+    h: CH,
+    w: CW * 0.18,
+    d: CW * 0.18,
     perspective: '1000px',
-    perspectiveOrigin: '-2500px 2300px',  // origin is center of screen
+    perspectiveOrigin: '-' + (CW / 2) + 'px ' + (CH / 2) + 'px',  // origin is center of screen
   });
-  corridor.right.css({backgroundImage: 'radial-gradient(at 60% 60%, rgba(184, 155, 176, 0.3) 10%, rgba(28, 17, 22, 0.46) 90%)'});
-  corridor.bottom.css({backgroundImage: 'radial-gradient(at 50% 65%, rgba(61, 54, 41, 0.3) 40%, rgba(28, 17, 22, 0.3) 140%)'});
-
-  var anotherRoom = makeRoom({
-    x: 300,
-    y: 0,
-    h: pixelHeight,
-    w: 850,
-    d: 800
-  });
-  anotherRoom.right.css({backgroundColor: 'red'});
-  anotherRoom.bottom.css({backgroundColor: 'blue'});
-
-  var rug = Thing.classes.Img.make({
-    x: 0,
-    y: 0,
-    z: 0,
-    rotate: {z: 0, y: 0},
-    src:'img/persian_carpet_fine_red_1.png'
-  });
+  corridor.room.right.css({backgroundImage: 'radial-gradient(at 60% 60%, rgba(184, 155, 176, 0.3) 10%, rgba(28, 17, 22, 0.46) 90%)'});
+  corridor.room.bottom.css({backgroundImage: 'radial-gradient(at 50% 65%, rgba(61, 54, 41, 0.3) 40%, rgba(28, 17, 22, 0.3) 140%)'});
 
   var innerRoom = makeRoom({
-    x: pixelWidth * 0.2,
-    y: 0,
-    w: pixelWidth/3,
-    h: pixelHeight,
+    x: CW * 0.2,
+    y: CH * 0.295,
+    z: CH * -0.2,
+    w: CW * 0.15,
+    h: CH * 0.7,
+    d: CW * 0.1,
     showOuter: true,
   });
   innerRoom.back.css({backgroundColor: 'rgba(0,0,0,.5)'});
@@ -168,15 +151,16 @@ $(function () {
   innerRoom.top.css({backgroundColor: 'rgba(0,0,255,.5)'});
   innerRoom.bottom.css({backgroundColor: 'rgba(0,255,0,1)'});
   innerRoom.rotate({y:-15});
-  innerRoom.back.add(rug);
   innerRoom.bottom.add(fillFloor());
+  innerRoom.back.add(lightSpot);
 
   var mainRoom = makeRoom({
     x: 0,
     y: 0,
-    w: pixelWidth,
-    h: pixelHeight,
-    perspectiveOrigin: (pixelWidth * 0.5) + 'px ' + (pixelHeight * 0.75) + 'px',  // origin is center of screen
+    d: CH * 0.9,
+    w: CW,
+    h: CH,
+    perspectiveOrigin: (CW * 0.5) + 'px ' + (CH * 0.75) + 'px',  // origin is center of screen
   });
   mainRoom.back.css({backgroundColor: 'transparent'});
   mainRoom.left.css({backgroundColor: 'rgba(255,255,0,.2)'});
@@ -184,12 +168,13 @@ $(function () {
   mainRoom.top.css({backgroundColor: 'rgba(0,0,255,.2)'});
   mainRoom.bottom.css({backgroundColor: 'rgba(0,255,0,.2)'});
   mainRoom.bottom.add(fillFloor());
+  mainRoom.back.add(corridor);
   mainRoom.add([
     innerRoom,
     legRoom,
   ]);
 
-  var background = Meninas.makeBackground(pixelWidth, pixelHeight)
+  var background = Meninas.makeBackground(CW, CH)
     .css({
       backgroundColor:'rgb(60, 47, 70)',
       backgroundImage: 'url(img/clouds_on_light_blue.jpg)',
@@ -198,11 +183,10 @@ $(function () {
   background.add([
     mainRoom,
   ]);
+  background.render();
 
   Thing.classes.Page.setScale(pageParams.scale || 1);
   Thing.classes.Page.initEvents();
-
-  background.render();
 
   // for debugging
   window.mainRoom = mainRoom;
